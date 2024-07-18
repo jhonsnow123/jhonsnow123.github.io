1374be223f7948b20349691dec5a095984753972
jhonsnow123 <jasonsnow1234@gmail.com>
2024-07-18 23:14:46 +0800

    Site updated: 2024-07-18 23:14:45

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/07/18/Hexo-doc/","2024/07/06/基本简介/","2024/07/18/搭建hexo博客/"];function toRandomPost(){
+var posts=["2024/07/06/基本简介/","2024/07/18/搭建hexo博客/","2024/07/18/Hexo-doc/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
