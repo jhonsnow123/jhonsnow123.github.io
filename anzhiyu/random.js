be398e088227dc1a60aa6fd41b0d404cf2abaab6
jhonsnow123 <jasonsnow1234@gmail.com>
2024-08-03 16:01:21 +0800

    Site updated: 2024-08-03 16:01:21

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/07/18/Hexo-doc/","2024/07/19/TCP协议深度解读/","2024/07/06/基本简介/","2024/07/18/搭建hexo博客/"];function toRandomPost(){
+var posts=["2024/07/18/Hexo-doc/","2024/07/30/区块链知识-Layer0-Layer1-Layer2到底是什么/","2024/07/19/TCP协议深度解读/","2024/07/06/基本简介/","2024/07/18/搭建hexo博客/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
